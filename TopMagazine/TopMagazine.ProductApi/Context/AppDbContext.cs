d5e52605887900a326cccbc4e8533e2e9acfa7c7
Jefferson Rodrigues <jeffersonrodrigues5030@gmail.com>
2024-01-05 11:35:39 -0300

    cria models product e category e appdbcontext

---
+++ TopMagazine/TopMagazine.ProductApi/Context/AppDbContext.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.EntityFrameworkCore;
+using TopMagazine.ProductApi.Models;
+
+namespace TopMagazine.ProductApi.Context
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options) { }
+
+        public DbSet<Category> Categories { get; set; }
+        public DbSet<Product> Products { get; set; }
+    }
+}
