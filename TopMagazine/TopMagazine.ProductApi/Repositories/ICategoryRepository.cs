7bfc1c173045cd2d50e538672ec95e983d39288a
Jefferson Rodrigues <jeffersonrodrigues5030@gmail.com>
2024-01-07 16:36:23 -0300

    cria camada de repository (product e category interface e implementação)

---
+++ TopMagazine/TopMagazine.ProductApi/Repositories/ICategoryRepository.cs
@@ -0,0 +1,13 @@
+﻿using TopMagazine.ProductApi.Models;
+
+namespace TopMagazine.ProductApi.Repositories;
+
+public interface ICategoryRepository
+{
+    Task<IEnumerable<Category>> GetAll();
+    Task<IEnumerable<Category>> GetCategoriesProducts();
+    Task<Category> GetById(int id);
+    Task<Category> Create(Category category);
+    Task<Category> Update(Category category);
+    Task<Category> Delete(int id);
+}
