35ba3c9ec9524f2b61e4df587d846331e35503e7
Jefferson Rodrigues <jeffersonrodrigues5030@gmail.com>
2024-01-07 10:10:27 -0300

    instala pacotes para automapper e injeção de dependencia e cria CategoryDTO e ProductDTO

---
+++ TopMagazine/TopMagazine.ProductApi/DTOs/CategoryDTO.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.DataAnnotations;
+using TopMagazine.ProductApi.Models;
+
+namespace TopMagazine.ProductApi.DTOs
+{
+    public class CategoryDTO
+    {
+        public int Id { get; set; }
+
+        [Required(ErrorMessage = "O nome é requerido")]
+        [MinLength(3)]
+        [MaxLength(100)]
+        public string? Name { get; set; }
+        public ICollection<Product>? Products { get; set; }
+    }
+}
