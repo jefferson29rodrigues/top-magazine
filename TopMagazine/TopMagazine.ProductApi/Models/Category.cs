9ade97a1e6fe9256647b3141f040e71bd02da766
Jefferson Rodrigues <jeffersonrodrigues5030@gmail.com>
2024-11-01 23:32:45 -0300

    Adicionar .gitignore

--- TopMagazine/TopMagazine.ProductApi/Models/Category.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace TopMagazine.ProductApi.Models;
-
-public class Category
-{
-    public int Id { get; set; }
-    public string? Name { get; set; }
-    public ICollection<Product>? Products { get; set; }
-}
