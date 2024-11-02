519b02b655133872df72657efef87f43471d919e
Jefferson Rodrigues <jeffersonrodrigues5030@gmail.com>
2024-11-01 23:36:02 -0300

    Adicionar .gitignore

---
+++ TopMagazine/TopMagazine.ProductApi/DTOs/Mappings/MappingProfile.cs
@@ -0,0 +1,14 @@
+﻿using AutoMapper;
+using TopMagazine.ProductApi.Models;
+
+namespace TopMagazine.ProductApi.DTOs.Mappings;
+
+public class MappingProfile : Profile
+{
+    public MappingProfile()
+    {
+        CreateMap<Category, CategoryDTO>().ReverseMap();
+
+        CreateMap<Product, ProductDTO>().ReverseMap();
+    }
+}
---
+++ TopMagazine/TopMagazine.ProductApi/Services/IProductService.cs
@@ -0,0 +1,14 @@
+﻿using TopMagazine.ProductApi.DTOs;
+
+namespace TopMagazine.ProductApi.Services
+{
+    public interface IProductService 
+    {
+        Task<IEnumerable<ProductDTO>> GetProducts();
+        Task<ProductDTO> GetProductById();
+        Task<ProductDTO> AddProduct(ProductDTO productDTO);
+        Task<ProductDTO> UpdateProduct(ProductDTO productDTO);
+        Task<ProductDTO> RemoveProduct(int id);
+
+    }
+}
